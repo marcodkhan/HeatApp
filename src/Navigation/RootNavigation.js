cd9a613aadd5613f79fbe63c02e0392ba27680df
marcodkhan <marco.dk.han@gmail.com>
2020-07-06 10:38:41 +0900

    code formatting

--- src/Navigation/RootNavigation.js
+++ src/Navigation/RootNavigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '~/Screens/HomeScreen';
 import StoredScreen from '~/Screens/StoredScreen';
 import ReleasedScreen from '~/Screens/ReleasedScreen';
@@ -9,9 +9,9 @@ const Stack = createStackNavigator();
 
 const headerStyle = {
   title: '메인',
-  headerStyle: {backgroundColor: '#f4511e'},
+  headerStyle: { backgroundColor: '#f4511e' },
   headerTintColor: '#fff',
-  headerTitleStyle: {fontWeight: 'bold'},
+  headerTitleStyle: { fontWeight: 'bold' },
 };
 
 const RootNavigation = () => {
@@ -22,17 +22,17 @@ const RootNavigation = () => {
           name="Home"
           component={HomeScreen}
           options={headerStyle}
-          initialParams={{itemId: 42}}
+          initialParams={{ itemId: 42 }}
         />
         <Stack.Screen
           name="Stored"
           component={StoredScreen}
-          options={{title: '입고'}}
+          options={{ title: '입고' }}
         />
         <Stack.Screen
           name="Released"
           component={ReleasedScreen}
-          options={{title: '출고'}}
+          options={{ title: '출고' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <>
       <TouchableOpacity
         onPress={() => navigation.navigate('Stored')}
-        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>입고</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => navigation.navigate('Released')}
-        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>출고</Text>
       </TouchableOpacity>
     </>
